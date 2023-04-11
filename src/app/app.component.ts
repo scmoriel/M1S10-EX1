5c6b3743586acd725084ef9433c046d3d15df726
Moriel da Silva Costa <morielsc1997@gmail.com>
2023-04-10 23:28:18 -0300

    Exercicio 1 da Semana 10

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'M1S10-Ex1';
+  check: boolean = false;
+  modeOn: boolean = false;
+}
